6e50103af9868c80c8dbc6cb09a075d687aa08c9
rivercome <897971480@qq.com>
2018-10-15 19:12:51 +0800

    [test]..

--- src/routes/Login/index.js
+++ src/routes/Login/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { List, InputItem, WhiteSpace } from 'antd-mobile';
-import { createForm } from 'rc-form';
+import { createForm } from 'rc-form'
 
 
 const isIPhone = new RegExp('\\biPhone\\b|\\biPod\\b', 'i').test(window.navigator.userAgent);
