2e27b10ccf0b0031d1f67dd1b5abf3a8930a31ba
waidzsalome <841854755@qq.com>
2018-10-21 20:05:59 +0800

    list组件

--- src/router.js
+++ src/router.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Router, Route, Switch } from 'dva/router';
-import IndexPage from './routes/IndexPage';
-import Login from './routes/Login/index'
-import submission from './components/submissionAdd'
+import React from "react";
+import { Router, Route, Switch } from "dva/router";
+import IndexPage from "./routes/IndexPage";
+import Login from "./routes/Login/index";
+import ListInform from "./components/List/list";
+import submission from "./components/submissionAdd";
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
-
-        <Switch>
-          <Route path="/" exact component={IndexPage} />
-          <Route path="/login"  component={Login} />
-          <Route path="/a" component={submission} />
-        </Switch>
-
+      <Switch>
+        <Route path="/" exact component={IndexPage} />
+        <Route path="/login" component={Login} />
+        <Route path="/a" component={submission} />
+        <Route path="/list" component={ListInform} />
+      </Switch>
     </Router>
   );
 }
