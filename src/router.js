b5ecdc56944f5a2dea1faef6bb5126c561dfd802
rivercome <897971480@qq.com>
2018-10-29 19:58:12 +0800

    [style]添加Layout基本样式

--- src/router.js
+++ src/router.js
@@ -3,7 +3,6 @@ import { Router, Route, Switch } from "dva/router";
 import IndexPage from "./routes/IndexPage";
 // import Layout from "../src/components/Layout/index";
 import Login from "./routes/Login/index";
-import Test from "./routes/test/index";
 import ListInform from "./components/List/list";
 import submission from "./components/submissionAdd";
 
@@ -13,7 +12,6 @@ function RouterConfig({ history }) {
       <Switch>
         <Route path="/" exact component={IndexPage} />
         <Route path="/login" component={Login} />
-        <Route path="/test" component={Test} />
         <Route path="/a" component={submission} />
         <Route path="/list" component={ListInform} />
       </Switch>
